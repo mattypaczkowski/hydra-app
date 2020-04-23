20d131e5ed638a29c2f857447e54f1a80aa6d77f
mattypaczkowski <mattypaczkowski@gmail.com>
2020-04-23 21:38:40 +0100

    change JS code

--- src/js/main.js
+++ src/js/main.js
@@ -18,28 +18,24 @@ if ('serviceWorker' in navigator) {
 
 
 // add queryselector to button add
-const addButtonSelector = document.querySelector('.waterApp__button--add')
+const addButtonSelector = document.querySelector('.waterApp__button--add');
 // add queryselector to button remove
-const removeButtonSelector = document.querySelector('.waterApp__button--remove')
+const removeButtonSelector = document.querySelector('.waterApp__button--remove');
 // add queryselector to count numbers
-const numbersSelector = document.querySelector('.waterApp__numbers')
-
+const value = document.querySelector('.waterApp__numbers');
+//curen data 
+const key = new Date().toISOString().slice(0, 10);
 
 addButtonSelector.addEventListener("click", e =>{
   ////add one glass more
-  const valueSelector = numbersSelector.innerHTML
-  if (valueSelector <99){
-  const number = Number(valueSelector)
-    const newNumber = number + 1;
-    numbersSelector.innerText = newNumber.toString();
-
+  if (value.innerHTML < 99) {
+  value.innerHTML = parseInt(value.innerHTML) + 1;
 }});
+
 
 removeButtonSelector.addEventListener("click", e =>{
   ////remove one glass more
-  const valueSelector = numbersSelector.innerHTML
-  if (valueSelector >=1) {
-  const number = Number(valueSelector)
-    const newNumber = number - 1;
-    numbersSelector.innerText = newNumber.toString();
+  if (value.innerHTML > 0) {
+  value.innerHTML = parseInt(value.innerHTML) - 1;
 }});
+
